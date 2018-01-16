25caa675d44023c617ba8138d3917ab89f9a545c
ohrabo <32614539+ohrabo@users.noreply.github.com>
2018-01-16 13:26:25 +0000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1yCAn7lO1p7dfgthDNu4BZQ6VVx7ZybpM9qunR97PbI8/edit#gid=0';
